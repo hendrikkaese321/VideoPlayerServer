defb09b0982db75b0930f7e9d1fe645755951631
hendrikkaese321 <58625304+hendrikkaese321@users.noreply.github.com>
2024-08-02 16:01:15 +0300

    optimizing

--- routes.js
+++ routes.js
@@ -2,21 +2,16 @@ const express = require('express');
 const router = express.Router();
 const videoController = require('./videoController');
 
-const asyncHandler = fn => 
-  (req, res, next) => 
-    Promise.resolve(fn(req, res, next))
-           .catch(next);
+// Wrapping async control to reduce boilerplate in route declarations
+const asyncHandler = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 
 router.post('/upload', asyncHandler(videoController.uploadVideo));
-
 router.put('/update/:id', asyncHandler(videoController.updateVideo));
-
 router.delete('/delete/:id', asyncHandler(videoController.deleteVideo));
-
 router.get('/videos', asyncHandler(videoController.getAllVideos));
-
 router.get('/video/:id', asyncHandler(videoController.getVideoById));
 
+// Unified error handling
 router.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send('Something went wrong!');
