7ed8662bfac1f53056dda82de5cb965d72f8dbf9
hendrikkaese321 <58625304+hendrikkaese321@users.noreply.github.com>
2024-08-09 07:15:13 +0300

    update with new function

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const Video = require('./videoModel'); 
+const Video = require('./videoModel');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const uploadVideo = async (req, res) => {
   const { title, description, url } = req.body;
@@ -10,6 +10,7 @@ const uploadVideo = async (req, res) => {
       title,
       description,
       url,
+      views: 0, // Ensure this is part of your Video model.
     });
     await video.save();
     res.status(201).json(video);
@@ -63,10 +64,42 @@ const deleteVideo = async (req, res) => {
   }
 };
 
+const incrementVideoViews = async (req, res) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      return res.status(404).json({ message: 'Video not found!' });
+    }
+    video.views = video.views + 1;
+    await video.save();
+    res.json(video);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   uploadVideo,
   getAllVideos,
   getVideoById,
   updateVideo,
   deleteVideo,
+  incrementVideoViews, // Exporting the new function.
 };
+```
+```javascript
+const express = require('express');
+const router = express.Router();
+const { uploadVideo, getAllVideos, getVideoById, updateVideo, deleteVideo, incrementVideoViews } = require('./yourControllerFile');
+
+// existing endpoints
+router.post('/videos', uploadVideo);
+router.get('/videos', getAllVideos);
+router.get('/videos/:id', getVideoById);
+router.put('/videos/:id', updateVideo);
+router.delete('/videos/:id', deleteVideo);
+
+// new endpoint for incrementing video views
+router.post('/videos/:id/views', incrementVideoViews);
+
+module.exports = router;
