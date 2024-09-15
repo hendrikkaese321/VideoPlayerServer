2274c5ff915d000982a31bf558706ba1087d98ef
hendrikkaese321 <58625304+hendrikkaese321@users.noreply.github.com>
2024-09-15 13:59:54 +0300

    add new variables

--- database/db.js
+++ database/db.js
@@ -1,13 +1,1 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const connectionString = process.env.MONGODB_URI;
-
-mongoose.connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected successfully.'))
-.catch(err => console.error('MongoDB connection error:', err));
-
-module.exports = mongoose.connection;
+```
